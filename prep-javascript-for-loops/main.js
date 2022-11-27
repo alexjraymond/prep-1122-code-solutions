841083d290ebeb9391e68542bcb54baa3878d673
Alex Raymond <alexjacobraymond@gmail.com>
2022-11-27 20:55:15 +0000

    im in the right folder this time

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,15 @@
+// create your loops here.
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+let str = 0;
+for (let i = 0; i < 10; i++) {
+  str = str + 2;
+  console.log(str);
+}
+
+for (let i = 100; i > 0; i--) {
+  console.log('Time till explosion: ' + i);
+}
